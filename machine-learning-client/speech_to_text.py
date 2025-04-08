b97511c9f497660b59b789b3b4f0e9b5810ebddc
mam10023 <mam10023@nyu.edu>
2025-04-08 08:38:13 -0400

    add docstring (pylint)

---
+++ machine-learning-client/speech_to_text.py
@@ -0,0 +1,21 @@
+"""Using Whisper from Open AI, converts an audio recording to text transcript."""
+
+import whisper  # also need to install ffmpeg
+
+
+def speech_to_text(recording):
+    """Whisper is used for Audio Recording to text"""
+
+    model = whisper.load_model(
+        "turbo"
+    )  # chose turbo from whisper's README, may have other options
+    transcript = model.transcribe(recording)
+
+    return transcript["text"]
+
+
+if __name__ == "__main__":
+    # need to get audio from somewhere
+    audio = record(5)  # need to implement something like this
+    transcription = speech_to_text(audio)
+    print(f"Transcription: {transcription}")
