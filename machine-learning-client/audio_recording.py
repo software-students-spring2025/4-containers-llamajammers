ee2f1193ae9e898d686cc25eca296e73fa7fb0b7
mam10023 <mam10023@nyu.edu>
2025-04-08 09:18:03 -0400

    audio recording feature and suppressing pylint errors

--- machine-learning-client/audio_recording.py
+++ machine-learning-client/audio_recording.py
@@ -1,14 +1,33 @@
 """Use PyAudio to record audio"""
 
-import pyaudio
 import wave
-import sys
+import pyaudio
 
-# from PyAudio Installation guide
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 1 if sys.platform == "darwin" else 2
-RATE = 44100
-RECORD_SECONDS = 5  # should we change to 1 minute?
 
-audio = pyaudio.PyAudio()
+def audio_recording(filename="recording.wav"):
+    """Use PyAudio for recording audio and saving as a wave file"""
+    # from PyAudio Installation guide
+    CHUNK = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 44100  # not sure what is optimal for audio recording
+    RECORD_SECONDS = 5  # should we change to 1 minute?
+
+    # pylint: disable=no-member
+    # ^ suppressing an error with pylint but i dont think this is an issue at runtime
+    with wave.open(filename, "wb") as wf:
+        audio = pyaudio.PyAudio()
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(audio.get_sample_size(FORMAT))
+        wf.setframerate(RATE)
+
+        stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True)
+
+        print("Recording...")
+        for _ in range(0, RATE // CHUNK * RECORD_SECONDS):
+            wf.writeframes(stream.read(CHUNK))
+
+    stream.close()
+    audio.terminate()
+
+    return filename
